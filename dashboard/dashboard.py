797d72a728f3313caabaa22c1153d92a4af40d32
zerippeur <zerippeur@protonmail.com>
2024-01-23 17:19:18 +0100

    Added layout for local feature importance implementation (not implemented yet), moved client_id input to sidebar, added a python file for custom dashboard functions

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,103 +1,12 @@
 import pandas as pd
 import streamlit as st
 import requests
-from client_infos import ClientInfosDebugSplit
 import sqlite3
 import matplotlib.pyplot as plt
+from sqlalchemy import create_engine, text
+from dashboard_functions import display_credit_result, plot_gauge, display_feature_importance, get_global_feature_importance, predict_credit_risk
 
-risk_categories = {
-    'SAFE': 'Darkturquoise',
-    'UNCERTAIN': 'Paleturquoise',
-    'RISKY': 'Gold',
-    'VERY RISKY': 'Coral',
-    'NOPE': 'Firebrick'
-}
-
-def plot_gauge(value, prediction):
-    colors = ['Darkturquoise', 'Paleturquoise', 'Gold', 'Coral', 'Firebrick']
-    values = [100, 80, 60, 40, 20, 0]
-    x_axis_values = [0, 0.628, 1.256, 1.884, 2.512, 3.14]
-
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(projection='polar')
-    ax.bar(x=x_axis_values[:-1], height=0.5, width=0.63, color=colors, bottom=2, align='edge', linewidth=3, edgecolor='white')
-
-    y_areas = 2.23
-    fontsize_areas = 18
-
-    plt.annotate("SAFE", xy=(0.314,y_areas), rotation=-72, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
-    plt.annotate("UNCERTAIN", xy=(0.942,y_areas), rotation=-36, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
-    plt.annotate("RISKY", xy=(1.57,y_areas), color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
-    plt.annotate("VERY RISKY", xy=(2.198,y_areas), rotation=36, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
-    plt.annotate("NOPE", xy=(2.826,y_areas), rotation=72, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
-
-    for loc, val in zip(x_axis_values, values):
-        plt.annotate(f'{val}%', xy=(loc, 2.5), va='bottom', ha='right' if val<=40 else 'left', fontsize=14)
-
-    gauge_value = value if prediction == 0 else 100 - value
-    gauge_position = 3.14-(value*0.0314) if prediction == 0 else 3.14-((100 - value)*0.0314)
-    plt.annotate(f'{gauge_value:.2f}%', xytext=(0,0), xy=(gauge_position, 2.0),
-             arrowprops=dict(arrowstyle="wedge, tail_width=0.5", color="black", shrinkA=0),
-             bbox=dict(boxstyle="circle", facecolor="black", linewidth=2.0, ),
-             fontsize=25, color="white", ha="center", va='center', fontweight="bold"
-            )
-
-
-    plt.title("Confidence meter", loc="center", pad=20, fontsize=20, fontweight="bold")
-
-    ax.set_axis_off()
-    st.pyplot(fig)
-
-def display_credit_result(prediction, confidence, risk_category):
-    font_color = risk_categories[risk_category]
-    st.markdown(f"## Credit approval: <span style='color:{font_color}'>{risk_category}</span>", unsafe_allow_html=True)
-
-    # Display confidence as a gauge
-    st.markdown("### Credit risk profile:")
-    plot_gauge(confidence * 100, prediction)
-
-st.set_page_config(
-    page_title='Credit risk prediction', page_icon=':clipboard:', layout='wide'
-)
-
-# Function to get feature importance from API
-def get_global_feature_importance():
-    # Replace this URL with your actual API endpoint
-    api_url = "http://127.0.0.1:8000/global_feature_importance"
-    
-    
-    # Send POST request to API
-    response = requests.post(api_url)
-
-    if response.status_code == 200:
-        result = response.json()
-        return result
-    else:
-        st.error("Error fetching feature importance from API")
-        return None
-    
-def display_feature_importance(model_type, nb_features, importance_type):
-    st.markdown("### Feature importance")
-    if model_type == 'XGBClassifier':
-        feature_importance = st.session_state['feature_importance']['feature_importance'][importance_type]
-        importance = importance_type
-    elif model_type == 'RandomForestClassifier':
-        feature_importance = st.session_state['feature_importance']['feature_importance']
-        importance = 'Importance'
-    else:
-        st.error("Error: Unsupported model type")
-        return
-
-    # Sort features by importance
-    top_features = sorted(feature_importance.items(), key=lambda x: x[1], reverse=True)[:nb_features]
-
-    # Create horizontal bar plot
-    fig = plt.figure(figsize=(10, 6*nb_features/10))
-    plt.barh(range(len(top_features) - 1, -1, -1), [x[1] for x in top_features], tick_label=[x[0] for x in top_features])
-    plt.xlabel(f'{importance}')
-    plt.ylabel('Feature')
-    plt.title(f'Top {nb_features} Features - {model_type}')
-    st.pyplot(fig)
+model_threshold = .5
 
 # streamlit run dashboard.py
 
@@ -105,7 +14,7 @@ st.title('Credit risk prediction')
 st.markdown('Get credit risk prediction for a client based on his/her ID')
 
 
-tab1, tab2, tab3 = st.tabs([':clipboard: Credit risk prediction', ':bar_chart: Feature importance', ':chart_with_upwards_trend: Client infos'])
+tab1, tab2, tab3 = st.tabs([':clipboard: Credit risk prediction', ':bar_chart: Feature importance', ':chart_with_upwards_trend: Client informations'])
 
 if 'client_id' not in st.session_state:
     st.session_state['client_id'] = None
@@ -113,60 +22,53 @@ if 'client_id' not in st.session_state:
 if 'feature_importance' not in st.session_state:
     st.session_state['feature_importance'] = get_global_feature_importance()
 
+client_id = st.sidebar.number_input(
+    label='Client ID', min_value=0, max_value=1000000, value=st.session_state['client_id'],
+    step=1, format='%i', placeholder='Enter client ID'    
+)
+submit_id = st.sidebar.button('Submit client ID', key='submit_id')
+if submit_id:
+    st.session_state['client_id'] = client_id
+
 with tab1:
-    client_id = st.number_input(
-        label='Client ID', min_value=0, max_value=1000000, value=st.session_state['client_id'],
-        step=1, format='%i', placeholder='Enter client ID'
-    )
+    st.write('Current client ID:', st.session_state['client_id'])
+
     submit = st.button('Predict credit risk', key='submit')
 
     if submit:
-        st.session_state['client_id'] = client_id
-        st.write('Current client ID:', st.session_state['client_id'])
 
-        conn = sqlite3.connect(
-            'C:/Users/emile/DEV/WORKSPACE/projet-7-cours-oc/model/model/features/clients_infos.db'
-        )
-        query = f"SELECT * FROM train_df_debug WHERE SK_ID_CURR={client_id}"
-        result = pd.read_sql_query(query, conn, index_col='SK_ID_CURR')
-        conn.close()
+        if client_id is None:
+            st.write('Please enter a client ID in the sidebar section.')
 
-        model_dict = result.drop(columns=['index', 'TARGET']).to_dict(orient='index')
-        client_infos = model_dict[client_id]
-        json_payload_predict_from_dict = client_infos
-
-        response = requests.post(
-            'http://127.0.0.1:8000/predict_from_dict', json=json_payload_predict_from_dict
-        )
-
-        if response.status_code == 200:
-            prediction_result = response.json()
-            prediction = prediction_result['prediction']
-            confidence = prediction_result['confidence']
-            risk_category = prediction_result['risk_category']
-
-            # Display prediction result
-            display_credit_result(prediction, confidence, risk_category)
+        else:
+            predict_credit_risk(client_id=client_id)
 
 with tab2:
     st.markdown('## Feature importance')
     st.write('Current client ID:', st.session_state['client_id'])
 
-    st.markdown('### Global feature importance')
+    feature_scale = st.radio('Select feature importance scale', ['Global', 'Local'], index=0, horizontal=True)
 
-    model_type = st.session_state['feature_importance']['model_type']
+    if feature_scale == 'Global':
+        st.markdown('### Global feature importance')
 
-    if st.session_state['feature_importance']['model_type'] == 'XGBClassifier':
-        importance_type = st.radio("Select importance type:", ["weight", "cover", "gain"], index=0)
-    elif st.session_state['feature_importance']['model_type'] == 'RandomForestClassifier':
-        pass
+        model_type = st.session_state['feature_importance']['model_type']
 
-    nb_features = st.number_input(
-        label='Features nb', min_value=0, max_value=30, value=20,
-        step=1, format='%i', placeholder='Enter number of features to display'
-    )
+        if st.session_state['feature_importance']['model_type'] == 'XGBClassifier':
+            importance_type = st.radio("Select importance type:", ["weight", "cover", "gain"], index=0, horizontal=True)
+        elif st.session_state['feature_importance']['model_type'] == 'RandomForestClassifier':
+            pass
 
-    display_feature_importance(model_type, nb_features, importance_type)
+        nb_features = st.number_input(
+            label='Features nb', min_value=0, max_value=30, value=20,
+            step=1, format='%i', placeholder='Enter number of features to display'
+        )
+
+        display_feature_importance(model_type, nb_features, importance_type)
+    else:
+        st.markdown('### Local feature importance')
+        if 'client_id' not in st.session_state:
+            st.write('Please select a client ID in the sidebar section.')
 
 with tab3:
     st.markdown('## Client infos')
---
+++ dashboard/dashboard_functions.py
@@ -0,0 +1,127 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+import requests
+import sqlite3
+import pandas as pd
+
+def plot_gauge(value, prediction):
+    colors = ['Darkturquoise', 'Paleturquoise', 'Gold', 'Coral', 'Firebrick']
+    values = [100, 80, 60, 40, 20, 0]
+    x_axis_values = [0, 0.628, 1.256, 1.884, 2.512, 3.14]
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection='polar')
+    ax.bar(x=x_axis_values[:-1], height=0.5, width=0.63, color=colors, bottom=2, align='edge', linewidth=3, edgecolor='white')
+
+    y_areas = 2.23
+    fontsize_areas = 18
+
+    plt.annotate("SAFE", xy=(0.314,y_areas), rotation=-72, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
+    plt.annotate("UNCERTAIN", xy=(0.942,y_areas), rotation=-36, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
+    plt.annotate("RISKY", xy=(1.57,y_areas), color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
+    plt.annotate("VERY RISKY", xy=(2.198,y_areas), rotation=36, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
+    plt.annotate("NOPE", xy=(2.826,y_areas), rotation=72, color="white", fontweight="bold", fontsize=fontsize_areas, va="center", ha="center")
+
+    for loc, val in zip(x_axis_values, values):
+        plt.annotate(f'{val}%', xy=(loc, 2.5), va='bottom', ha='right' if val<=40 else 'left', fontsize=14)
+
+    gauge_value = value if prediction == 0 else 100 - value
+    gauge_position = 3.14-(value*0.0314) if prediction == 0 else 3.14-((100 - value)*0.0314)
+    plt.annotate(f'{gauge_value:.2f}%', xytext=(0,0), xy=(gauge_position, 2.0),
+             arrowprops=dict(arrowstyle="wedge, tail_width=0.5", color="black", shrinkA=0),
+             bbox=dict(boxstyle="circle", facecolor="black", linewidth=2.0, ),
+             fontsize=25, color="white", ha="center", va='center', fontweight="bold"
+            )
+
+
+    plt.title("Confidence meter", loc="center", pad=20, fontsize=20, fontweight="bold")
+
+    ax.set_axis_off()
+    st.pyplot(fig)
+
+def display_credit_result(prediction, confidence, risk_category, threshold=.5):
+
+    risk_categories = {
+        'SAFE': 'Darkturquoise',
+        'UNCERTAIN': 'Paleturquoise',
+        'RISKY': 'Gold',
+        'VERY RISKY': 'Coral',
+        'NOPE': 'Firebrick'
+    }
+
+    font_color = risk_categories[risk_category]
+    chance_percentage = confidence * 100 if prediction == 0 else 100 - confidence * 100
+    st.markdown(f"## Credit approval: <span style='color:{font_color}'>{risk_category}</span>", unsafe_allow_html=True)
+    st.markdown(f"### According to our prediction model,"
+                f" you have a <span style='color:{font_color}'>{chance_percentage:.2f}%</span>"
+                " chance to repay your loan.\n"
+                f"### Our threshold is fixed at {threshold*100:.2f}%."
+                " Please see feature importance or client informations for more information.",
+                unsafe_allow_html=True)
+
+    # Display confidence as a gauge
+    st.markdown("### Credit risk profile:")
+    plot_gauge(confidence * 100, prediction)
+
+# Function to get feature importance from API
+def get_global_feature_importance(api_url="http://127.0.0.1:8000/global_feature_importance"):
+    # Replace this URL with your actual API endpoint
+    api_url = api_url
+    
+    # Send POST request to API
+    response = requests.post(api_url)
+
+    if response.status_code == 200:
+        result = response.json()
+        return result
+    else:
+        st.error("Error fetching feature importance from API")
+        return None
+    
+def display_feature_importance(model_type, nb_features, importance_type):
+    st.markdown("### Feature importance")
+    if model_type == 'XGBClassifier':
+        feature_importance = st.session_state['feature_importance']['feature_importance'][importance_type]
+        importance = importance_type
+    elif model_type == 'RandomForestClassifier':
+        feature_importance = st.session_state['feature_importance']['feature_importance']
+        importance = 'Importance'
+    else:
+        st.error("Error: Unsupported model type")
+        return
+
+    # Sort features by importance
+    top_features = sorted(feature_importance.items(), key=lambda x: x[1], reverse=True)[:nb_features]
+
+    # Create horizontal bar plot
+    fig = plt.figure(figsize=(10, 6*nb_features/10))
+    plt.barh(range(len(top_features) - 1, -1, -1), [x[1] for x in top_features], tick_label=[x[0] for x in top_features])
+    plt.xlabel(f'{importance}')
+    plt.ylabel('Feature')
+    plt.title(f'Top {nb_features} Features - {model_type}')
+    st.pyplot(fig)
+
+def predict_credit_risk(client_id: int, threshold: float = .5, api_url="http://127.0.0.1:8000/predict_from_dict"):
+    conn = sqlite3.connect(
+        'C:/Users/emile/DEV/WORKSPACE/projet-7-cours-oc/model/model/features/clients_infos.db'
+    )
+    query = f"SELECT * FROM train_df_debug WHERE SK_ID_CURR = ?"
+    result = pd.read_sql_query(query, conn, params=[client_id], index_col='SK_ID_CURR')
+    conn.close()
+
+    model_dict = result.drop(columns=['index', 'TARGET']).to_dict(orient='index')
+    client_infos = model_dict[client_id]
+    json_payload_predict_from_dict = client_infos
+
+    response = requests.post(
+        'http://127.0.0.1:8000/predict_from_dict', json=json_payload_predict_from_dict
+    )
+
+    if response.status_code == 200:
+        prediction_result = response.json()
+        prediction = prediction_result['prediction']
+        confidence = prediction_result['confidence']
+        risk_category = prediction_result['risk_category']
+
+        # Display prediction result
+        display_credit_result(prediction, confidence, risk_category, threshold=threshold)
