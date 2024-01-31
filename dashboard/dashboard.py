16fe27291de9b573c13cc526fc56f140680e7104
zerippeur <zerippeur@protonmail.com>
2024-01-31 21:59:52 +0100

    Working shap feature importance, global and local

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,9 @@ import requests
 import sqlite3
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine, text
-from dashboard_functions import display_credit_result, plot_gauge, display_feature_importance, get_global_feature_importance, predict_credit_risk
+from dashboard_functions import display_credit_result, plot_gauge, display_built_in_global_feature_importance, get_built_in_global_feature_importance, predict_credit_risk
+from dashboard_functions import initiate_shap_explainer, get_shap_feature_importance, display_shap_feature_importance
+from streamlit_shap import st_shap
 
 model_threshold = .5
 
@@ -14,13 +16,29 @@ st.title('Credit risk prediction')
 st.markdown('Get credit risk prediction for a client based on his/her ID')
 
 
-tab1, tab2, tab3 = st.tabs([':clipboard: Credit risk prediction', ':bar_chart: Feature importance', ':chart_with_upwards_trend: Client informations'])
+tab1, tab2, tab3, tab4 = st.tabs([':clipboard: Credit risk prediction', ':bar_chart: Feature importance', ':chart_with_upwards_trend: Client informations', ':wrench: debug'])
 
 if 'client_id' not in st.session_state:
     st.session_state['client_id'] = None
 
 if 'feature_importance' not in st.session_state:
-    st.session_state['feature_importance'] = get_global_feature_importance()
+    st.session_state['feature_importance'] = get_built_in_global_feature_importance()
+
+if 'shap_explainer_initiated' not in st.session_state:
+    st.session_state['shap_explainer_initiated'] = 'Not initiated'
+
+if 'shap' not in st.session_state:
+    st.session_state['shap'] = {
+        'Global': {
+            'initiated': False,
+            'data': None
+        },
+        'Local': {
+            'initiated': False,
+            'data': None,
+            'client_id': None
+        }
+    }
 
 client_id = st.sidebar.number_input(
     label='Client ID', min_value=0, max_value=1000000, value=None,
@@ -30,11 +48,19 @@ submit_id = st.sidebar.button('Submit client ID', key='submit_id')
 if submit_id:
     st.session_state['client_id'] = client_id
 
+
+shap_initiation = st.sidebar.button('Initiate Shap explainer', key='initiate_shap_explainer', disabled=st.session_state['shap_explainer_initiated'] == 'Initiated')
+if shap_initiation:
+    initiate_shap_explainer()
+    st.session_state['shap_explainer_initiated'] = 'Initiated'
+    # refresh app
+    st.rerun()
+
 with tab1:
     st.write('Current client ID:', st.session_state['client_id'])
 
     if st.session_state['client_id'] is not None:
-        predict_credit_risk(client_id=client_id)
+        predict_credit_risk(client_id=st.session_state['client_id'])
     else:
         st.write('Please enter a client ID in the sidebar section.')
 
@@ -42,10 +68,10 @@ with tab2:
     st.markdown('## Feature importance')
     st.write('Current client ID:', st.session_state['client_id'])
 
-    feature_scale = st.radio('Select feature importance scale', ['Global', 'Local'], index=0, horizontal=True)
+    explainer = st.radio('Select explainer', ['Built_in', 'Shap'], index=None, horizontal=True)
 
-    if feature_scale == 'Global':
-        st.markdown('### Global feature importance')
+    if explainer == 'Built_in':
+        st.markdown('### Built_in feature importance')
 
         model_type = st.session_state['feature_importance']['model_type']
 
@@ -59,14 +85,34 @@ with tab2:
             step=1, format='%i', placeholder='Enter number of features to display'
         )
 
-        display_feature_importance(model_type, nb_features, importance_type)
-    else:
-        st.markdown('### Local feature importance')
-        if 'client_id' not in st.session_state:
-            st.write('Please select a client ID in the sidebar section.')
+        display_built_in_global_feature_importance(model_type, nb_features, importance_type)
+    elif explainer == 'Shap':
+        st.markdown('### Shap feature importance')
+
+        if st.session_state.shap_explainer_initiated == 'Not initiated':
+            st.write('Please initiate Shap explainer in the sidebar section.')
+
+        if st.session_state.shap_explainer_initiated == 'Initiated':
+            feature_scale = st.radio('Select shap feature importance scale', ["Global", "Local"], index=0, horizontal=True)
+            if feature_scale == 'Global':
+                nb_features = st.number_input(
+                    label='Features nb', min_value=0, max_value=30, value=20,
+                    step=1, format='%i', placeholder='Enter number of features to display'
+                )
+                display_shap_feature_importance(client_id=st.session_state['client_id'], scale=feature_scale, nb_features=nb_features)
+            elif feature_scale == 'Local':
+                if st.session_state['client_id'] is None:
+                    st.write('Please enter a client ID in the sidebar section.')
+                else:
+                    display_shap_feature_importance(client_id=st.session_state['client_id'], scale=feature_scale)
+                
 
 with tab3:
     st.markdown('## Client infos')
     st.write('Current client ID:', st.session_state['client_id'])
 
+with tab4:
+    st.markdown('## Debug')
+    st.write('Current client ID:', st.session_state['client_id'])
 
+    st.markdown('### No current debug available')
--- dashboard/dashboard_functions.py
+++ dashboard/dashboard_functions.py
@@ -3,12 +3,20 @@ import streamlit as st
 import requests
 import sqlite3
 import pandas as pd
+from typing import Tuple, Literal, Union
+from imblearn.over_sampling import SMOTE
+from imblearn .under_sampling import RandomUnderSampler
+from imblearn.pipeline import Pipeline
+import shap
+import numpy as np
+from streamlit_shap import st_shap
 
-def plot_gauge(value, prediction):
+def plot_gauge(value, prediction)-> None:
     colors = ['Darkturquoise', 'Paleturquoise', 'Gold', 'Coral', 'Firebrick']
     values = [100, 80, 60, 40, 20, 0]
     x_axis_values = [0, 0.628, 1.256, 1.884, 2.512, 3.14]
 
+    plt.clf()
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(projection='polar')
     ax.bar(x=x_axis_values[:-1], height=0.5, width=0.63, color=colors, bottom=2, align='edge', linewidth=3, edgecolor='white')
@@ -39,7 +47,7 @@ def plot_gauge(value, prediction):
     ax.set_axis_off()
     st.pyplot(fig)
 
-def display_credit_result(prediction, confidence, risk_category, threshold=.5):
+def display_credit_result(prediction, confidence, risk_category, threshold=.5)-> None:
 
     risk_categories = {
         'SAFE': 'Darkturquoise',
@@ -64,7 +72,7 @@ def display_credit_result(prediction, confidence, risk_category, threshold=.5):
     plot_gauge(confidence * 100, prediction)
 
 # Function to get feature importance from API
-def get_global_feature_importance(api_url="http://127.0.0.1:8000/global_feature_importance"):
+def get_built_in_global_feature_importance(api_url="http://127.0.0.1:8000/global_feature_importance")-> dict | None:
     # Replace this URL with your actual API endpoint
     api_url = api_url
     
@@ -78,7 +86,7 @@ def get_global_feature_importance(api_url="http://127.0.0.1:8000/global_feature_
         st.error("Error fetching feature importance from API")
         return None
     
-def display_feature_importance(model_type, nb_features, importance_type):
+def display_built_in_global_feature_importance(model_type, nb_features, importance_type)-> None:
     st.markdown("### Feature importance")
     if model_type == 'XGBClassifier':
         feature_importance = st.session_state['feature_importance']['feature_importance'][importance_type]
@@ -94,6 +102,7 @@ def display_feature_importance(model_type, nb_features, importance_type):
     top_features = sorted(feature_importance.items(), key=lambda x: x[1], reverse=True)[:nb_features]
 
     # Create horizontal bar plot
+    plt.clf()
     fig = plt.figure(figsize=(10, 6*nb_features/10))
     plt.barh(range(len(top_features) - 1, -1, -1), [x[1] for x in top_features], tick_label=[x[0] for x in top_features])
     plt.xlabel(f'{importance}')
@@ -101,8 +110,24 @@ def display_feature_importance(model_type, nb_features, importance_type):
     plt.title(f'Top {nb_features} Features - {model_type}')
     st.pyplot(fig)
 
-def predict_credit_risk(client_id: int, threshold: float = .5, api_url="http://127.0.0.1:8000/predict_from_dict"):
-    client_infos = get_client_infos(client_id=client_id)
+def get_client_infos(client_id: int, output: Literal['dict', 'df'] = 'df')-> dict | pd.DataFrame:
+    conn = sqlite3.connect(
+        'C:/Users/emile/DEV/WORKSPACE/projet-7-cours-oc/model/model/features/clients_infos.db'
+    )
+    query = f'SELECT * FROM train_df_debug WHERE SK_ID_CURR = ? ORDER BY "index"'
+    result = pd.read_sql_query(query, conn, params=[client_id], index_col='SK_ID_CURR')
+    conn.close()
+
+    if output == 'dict':
+        model_dict = result.drop(columns=['index', 'TARGET']).to_dict(orient='index')
+        client_infos = model_dict[client_id]
+    elif output == 'df':
+        client_infos = result.drop(columns=['index', 'TARGET'])
+    return client_infos
+
+
+def predict_credit_risk(client_id: int, threshold: float = .5, api_url="http://127.0.0.1:8000/predict_from_dict")-> None:
+    client_infos = get_client_infos(client_id=client_id, output='dict')
     json_payload_predict_from_dict = client_infos
 
     response = requests.post(
@@ -118,15 +143,132 @@ def predict_credit_risk(client_id: int, threshold: float = .5, api_url="http://1
         # Display prediction result
         display_credit_result(prediction, confidence, risk_category, threshold=threshold)
 
-def get_client_infos(client_id: int):
+def balance_classes(X: pd.DataFrame, y: pd.Series, method: Literal['smote', 'randomundersampler']='randomundersampler')-> Tuple[pd.DataFrame, pd.Series]:
+    """
+    Balance classes in the dataset using SMOTE or RandomUnderSampler.
+
+    Args:
+        X: Features. Accepts either a pandas DataFrame or a numpy array.
+        y: Target variable. Accepts either a pandas Series or a numpy array.
+        method: Method to use for balancing. Options: 'smote' or 'randomundersampler'.
+            Defaults to 'smote'.
+
+    Returns:
+        Balanced feature set and target variable. Returns a tuple containing the balanced feature set and target variable,
+        which can be either a pandas DataFrame or a numpy array depending on the input types.
+    """
+    sampler_dict = {'smote': SMOTE(sampling_strategy='auto', random_state=42),
+                    'randomundersampler': RandomUnderSampler(sampling_strategy='auto', random_state=42)}
+    sampler = sampler_dict.get(method.lower())
+    
+    if sampler is None:
+        raise ValueError("Invalid method. Choose 'smote' or 'randomundersampler'.")
+    
+    pipeline = Pipeline([('sampler', sampler)])
+    X_resampled, y_resampled = pipeline.named_steps['sampler'].fit_resample(X, y)
+    # return pipeline.fit_resample(X, y)
+    return X_resampled, y_resampled
+
+def get_data_for_shap_initiation()-> dict:
     conn = sqlite3.connect(
         'C:/Users/emile/DEV/WORKSPACE/projet-7-cours-oc/model/model/features/clients_infos.db'
     )
-    query = f'SELECT * FROM train_df_debug WHERE SK_ID_CURR = ? ORDER BY "index"'
-    result = pd.read_sql_query(query, conn, params=[client_id], index_col='SK_ID_CURR')
+    query = f'SELECT * FROM train_df_debug'
+    result = pd.read_sql_query(query, conn, index_col='SK_ID_CURR')
     conn.close()
 
-    model_dict = result.drop(columns=['index', 'TARGET']).to_dict(orient='index')
-    client_infos = model_dict[client_id]
+    X_train, y_train = result.drop(columns=['index', 'TARGET']), result['TARGET']
+    X_train_resampled, _ = balance_classes(X_train, y_train, method='randomundersampler')
 
-    return client_infos
+    data_for_shap_initiation = X_train_resampled.to_dict(orient='index')
+
+    return data_for_shap_initiation
+
+def initiate_shap_explainer(api_url="http://127.0.0.1:8000/initiate_shap_explainer")-> None:
+    data_for_shap_initiation = get_data_for_shap_initiation()
+    response = requests.post(api_url, json=data_for_shap_initiation)
+    if response.status_code == 200:
+        st.session_state.shap_explainer_initiated = True
+        st.success("Shap explainer initiated successfully")
+    else:
+        st.error("Error initiating shap explainer")
+        st.session_state.shap_explainer_initiated = False
+
+def get_feature_names_in(api_url="http://127.0.0.1:8000/feature_names_in")-> Union[dict, None]:
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        result = response.json()
+        return result
+    else:
+        st.error("Error fetching feature names from API")
+        return None
+
+def get_shap_feature_importance(client_id: int|None, scale: Literal['Global', 'Local'], api_url: str="http://127.0.0.1:8000/shap_feature_importance")-> Union[dict, None]:
+    if scale == 'Global':
+        json_payload_shap_feature_importance = {
+            'client_infos': None,
+            'feature_scale': scale
+        }
+    elif scale == 'Local':    
+        client_infos = get_client_infos(client_id=client_id, output='dict')
+        json_payload_shap_feature_importance = {
+            'client_infos': client_infos,
+            'feature_scale': scale
+        }
+
+    response = requests.post(
+        api_url, json=json_payload_shap_feature_importance
+    )
+
+    if response.status_code == 200:
+        result = response.json()
+        return result
+    else:
+        st.error("Error fetching feature importance from API")
+        return None
+
+def update_shap_session_state(client_id, scale, data):
+    st.session_state['shap'][scale]['initiated'] = True
+    st.session_state['shap'][scale]['data'] = data
+    st.session_state['shap'][scale]['client_id'] = client_id
+
+def plot_shap(shap_feature_importance_dict, scale, nb_features: int=20):
+    data = pd.DataFrame.from_dict(shap_feature_importance_dict['data'], orient='index')
+    shap_values = np.array(shap_feature_importance_dict['shap_values'])
+    feature_names = np.array(shap_feature_importance_dict['feature_names'])
+    expected_value = shap_feature_importance_dict['expected_value']
+    if scale == 'Global':
+        plt.clf()
+        st_shap(shap.summary_plot(
+            shap_values, #Use Shap values array
+            features=data, # Use training set features
+            feature_names=feature_names, #Use column names
+            show=False, #Set to false to output to folder
+            max_display=nb_features, # Set max features to display
+            plot_size=(10,10)) # Change plot size
+        )
+    elif scale == 'Local':
+        plt.clf()
+        st_shap(shap.force_plot(
+            expected_value, 
+            shap_values, 
+            features=data,
+            feature_names=data.columns,
+            show=False)
+        )
+
+def display_shap_feature_importance(client_id: int|None, scale: Literal['Global', 'Local'], nb_features: int=20)-> None:
+    if scale == 'Global':
+        if not st.session_state['shap'][scale]['initiated']:
+            shap_feature_importance_dict = get_shap_feature_importance(client_id=client_id, scale=scale, api_url="http://127.0.0.1:8000/shap_feature_importance")
+            plot_shap(shap_feature_importance_dict, scale=scale, nb_features=nb_features)
+            update_shap_session_state(client_id=client_id, scale=scale, data=shap_feature_importance_dict)
+        else:
+            plot_shap(st.session_state['shap'][scale]['data'], scale=scale, nb_features=nb_features)
+    elif scale == 'Local':
+        if not st.session_state['shap'][scale]['initiated'] or client_id is not st.session_state['shap'][scale]['client_id']:
+            shap_feature_importance_dict = get_shap_feature_importance(client_id=client_id, scale=scale, api_url="http://127.0.0.1:8000/shap_feature_importance")
+            plot_shap(shap_feature_importance_dict, scale=scale, nb_features=nb_features)
+            update_shap_session_state(client_id=client_id, scale=scale, data=shap_feature_importance_dict)
+        else:
+            plot_shap(st.session_state['shap'][scale]['data'], scale=scale, nb_features=nb_features)
