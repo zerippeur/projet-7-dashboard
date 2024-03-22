5afa199d28ce404e96f148a6a49a2f3b5152d4ce
zerippeur <zerippeur@protonmail.com>
2024-03-22 10:57:37 +0100

    changed stup/sh.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
-[theme]\n\
-base = 'light'\n\
-\n\
 " > ~/.streamlit/config.toml
